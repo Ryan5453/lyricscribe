94aac79cd9649e585cc225128c8c140cff7f85cc
Ryan <git@ryan.science>
2024-11-05 22:01:30 -0500

    fix slurm scripts

--- scripts/slurm-scripts/run_whisper_large-v3-turbo.sh
+++ scripts/slurm-scripts/run_whisper_large-v3-turbo.sh
@@ -2,19 +2,22 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=24G
-#SBATCH --gres=gpu:1
-#SBATCH --constraint="t4|v100-pcie|v100-sxm2" # Kepler GPUs have been causing me issues
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+# Change this to the GPU you want to use
+# I couldn't get the --constraint option to work...
+#SBATCH --gres=gpu:XXX:1
 #SBATCH --job-name=whisper_large-v3-turbo
-#SBATCH --output=~/music2text/output/whisper_large-v3-turbo_normal.out
-#SBATCH --time=08:00:00
+#SBATCH --output=/path/to/your/output/whisper_large-v3-turbo.out
+#SBATCH --time=08:00:00 
 
 module load miniconda3/23.11.0
 module load ffmpeg/20190305
 
-source ~/music2text/.venv/bin/activate
+source ${PWD}/.venv/bin/activate
 
+# Faster Whisper uses a CTranslate2 backend, which requires these CUDA libraries
+# I installed them via pip, so we have to add them to the LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=${PWD}/.venv/lib64/python3.11/site-packages/nvidia/cublas/lib:${PWD}/.venv/lib64/python3.11/site-packages/nvidia/cudnn/lib
 
-python whisper-wer.py --directory /path/to/your/audio --model large-v3-turbo
+python scripts/whisper-wer.py --directory /path/to/your/audio --model deepdml/faster-whisper-large-v3-turbo-ct2
