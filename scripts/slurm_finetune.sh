531872c690deb45a8a7e51733d076ec3522c5236
Ryan Fahey <git@ryan.science>
2026-04-21 01:16:21 -0400

    it worked

--- scripts/slurm_finetune.sh
+++ scripts/slurm_finetune.sh
@@ -31,10 +31,6 @@ module load FFmpeg/7.1.1
 export HF_HOME=/projects/fahey.rya/music2text/.cache/huggingface
 export TORCH_HOME=/projects/fahey.rya/music2text/.cache/torch
 export NEMO_CACHE_DIR=/projects/fahey.rya/music2text/.cache/nemo
-export NCCL_NVLS_ENABLE=0
-export NCCL_P2P_DISABLE=1
-export NCCL_DEBUG=INFO
-export NCCL_DEBUG_SUBSYS=INIT,COLL,NET
 
 cd /projects/fahey.rya/music2text/lyricscribe
 source .venv/bin/activate
